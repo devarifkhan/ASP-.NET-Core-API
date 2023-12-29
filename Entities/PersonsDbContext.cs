afc042b10df6b365dfc2183fcd0cea59401dc424
Md Ariful Islam <arifcse209@gmail.com>
2023-12-29 12:24:01 +0600

    data seed implemented

--- Entities/PersonsDbContext.cs
+++ Entities/PersonsDbContext.cs
@@ -18,6 +18,31 @@ namespace Entities
 
             modelBuilder.Entity<Country>().ToTable("Countries");
             modelBuilder.Entity<Person>().ToTable("Persons");
+
+            //Seed to Countries
+
+            string countriesJson = System.IO.File.ReadAllText(
+                "coutries.json");
+
+            List<Country>? countries=
+            System.Text.Json.JsonSerializer.Deserialize<List<Country>>(countriesJson);
+
+
+            foreach(Country country in countries)
+            modelBuilder.Entity<Country>().HasData(country);
+
+
+            //Seed to Persons
+
+            string personsJson = System.IO.File.ReadAllText(
+                "persons.json");
+
+            List<Person>? persons =
+            System.Text.Json.JsonSerializer.Deserialize<List<Person>>(personsJson);
+
+
+            foreach (Person person in persons)
+                modelBuilder.Entity<Person>().HasData(person);
         }
 
     }
