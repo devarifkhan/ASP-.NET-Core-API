61b620a2c2f220dc766dfa490cf32705f403c99b
Md Ariful Islam <arifcse209@gmail.com>
2023-12-29 10:53:24 +0600

    tag-helpers implemented

--- CRUDTests/obj/Debug/net8.0/CRUDTests.AssemblyInfo.cs
+++ CRUDTests/obj/Debug/net8.0/CRUDTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUDTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8584ef0fc8f893cbb1634a7462629b62e62e9050")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e9e588d2403987deb846a5971da18bad64c3a68")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUDTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUDTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
